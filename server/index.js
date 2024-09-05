9083533d2a26013332b66378ff2d083be9abb4e4
Maanav Nair <maanavnair20042@gmail.com>
2024-09-05 11:14:41 +0530

    backend setup

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+    },
+});
+
+server.listen(3001, () => {
+    console.log("Server Running");
+})
