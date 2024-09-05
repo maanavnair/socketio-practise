be2b6d9ae0a8b04dff1beda52b5316f55a99fc4a
Maanav Nair <maanavnair20042@gmail.com>
2024-09-05 16:28:26 +0530

    broadcast done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import io from "socket.io-client";
 const socket = io.connect("http://localhost:3001");
 
 const App = () => {
 
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+
 
   const sendMessage = () => {
-    socket.emit("send_message", {message: "Hello"})
+    socket.emit("send_message", { message });
   }
 
   useEffect(() => {
     socket.on("receive_message", (data) => {
-      alert(data.message);
+      setMessageReceived(data.message);
     })
   }, [socket])
 
@@ -19,10 +22,13 @@ const App = () => {
     <div>
       <input 
         placeholder="Message..."
+        onChange={(e) => setMessage(e.target.value)}
       />
       <button onClick={sendMessage}>
         Send Message
       </button>
+      <h1>Message:</h1>
+      {messageReceived}
     </div>
   )
 }
