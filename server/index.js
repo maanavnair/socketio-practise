1451e9b98a22e2be735c252591c4d09f81938078
Maanav Nair <maanavnair20042@gmail.com>
2024-09-05 16:17:27 +0530

    added socket

--- server/index.js
+++ server/index.js
@@ -16,6 +16,14 @@ const io = new Server(server, {
     },
 });
 
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+
+    socket.on("send_message", (data)=> {
+        socket.broadcast.emit("receive_message", data);
+    })
+})
+
 server.listen(3001, () => {
     console.log("Server Running");
 })
