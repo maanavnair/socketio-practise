68bd972655a86ffcb65749165351bc8680e4402f
Maanav Nair <maanavnair20042@gmail.com>
2024-09-05 16:17:04 +0530

    added socket

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import io from "socket.io-client";
+const socket = io.connect("http://localhost:3001");
 
 const App = () => {
+
+
+  const sendMessage = () => {
+    socket.emit("send_message", {message: "Hello"})
+  }
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      alert(data.message);
+    })
+  }, [socket])
+
   return (
     <div>
       <input 
         placeholder="Message..."
       />
-      <button>
+      <button onClick={sendMessage}>
         Send Message
       </button>
     </div>
